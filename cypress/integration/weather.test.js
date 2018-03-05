6d9ee67caf074a9848c27fa3e5934388b5c077ac
dzejzi <justynazubala@gmail.com>
2018-03-05 15:50:42 +0100

    cypress tests

--- cypress/integration/weather.test.js
+++ cypress/integration/weather.test.js
@@ -1,25 +1,90 @@
 describe('Weather Application', function () {
     //run cors before test
+    it('should contains form elements', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form')
+        cy.get('input.LoginForm__form__input')
+        cy.get('button.LoginForm__form__button')
+    });
+
+    it('should show error messages', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('qwerty');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('uiop');
+        cy.contains('Submit').click(); 
+        cy.get('form.LoginForm__form span').contains('Please enter'); 
+    });
+
+    it('should login to application', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+    });
+
+    it('should show cookieBanner', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.clearLocalStorage()
+        cy.get('div.CookieBanner')
+    });
+
+    it('should not show cookieBanner', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.clearLocalStorage();
+        cy.contains('Accept').click();
+        cy.get('div').not('CookieBanner')
+    });
+
+
     it('should contains input element', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
         cy.get('.AppInputSearch input')
     })
 
     it('should show loader when entering text', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
         cy.get('.AppInputSearch input').type('London');
         cy.get('svg.lds-spinner');
         //cy.wait(5000)
     });
 
     it('should show list with search results', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
         cy.get('.AppInputSearch input').type('London');
         cy.get('.AppInputSearch ul').contains('London')
     })
 
     it('should click to li and path should include /city/XX/London', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
         cy.get('.AppInputSearch input').type('London');
         cy.contains('London').click();
         cy.url().should('include', '/city');
@@ -27,11 +92,27 @@ describe('Weather Application', function () {
     })
 
     it('should come back to homepage', () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
         cy.get('.AppInputSearch input').type('London');
         cy.contains('London').click();
         cy.get('.City__iconWrapper__icon').click()
         cy.url().should('be', '/')
+    })
+
+    it('after refresh it should visit login', () => {
+        cy.visit('http://localhost:3000/login')
+        cy.get('form.LoginForm__form input:first').type('akra');
+        cy.get('form.LoginForm__form input:nth-child(3)').type('akra');
+        cy.contains('Submit').click(); 
+        cy.url().should('be', '/') 
+        cy.contains('Accept').click();
+        cy.reload();
+        cy.url().should('be', 'http://localhost:3000/login') 
     })
 
 })
--- src/routes/App/CookieBanner/CookieBanner.js
+++ src/routes/App/CookieBanner/CookieBanner.js
@@ -17,11 +17,9 @@ class CookieBanner extends Component {
                     <p className="CookieBanner__p">
                         Informujemy, iż w celu optymalizacji treści dostępnych w naszym serwisie, dostosowania ich do Państwa indywidualnych potrzeb korzystamy z informacji zapisanych za pomocą plików cookies na urządzeniach końcowych użytkowników.
             <br /> Pliki cookies użytkownik może kontrolować za pomocą ustawień swojej przeglądarki internetowej. Dalsze korzystanie z naszego serwisu internetowego, bez zmiany ustawień przeglądarki internetowej oznacza, iż użytkownik akceptuje stosowanie plików cookies. </p>
-                    <button className="CookieBanner__button" onClick={handleOnClick}>Akceptuję</button>
+                    <button className="CookieBanner__button" onClick={handleOnClick}>Accept</button>
                 </div>
             )
-
-
         )
     }
 }
